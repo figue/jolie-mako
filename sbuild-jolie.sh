24d42a0ba28d085771a54b1c804fa595e97f3f3f
Figue <ffigue@gmail.com>
2014-05-27 00:15:21 +0200

    Updated to v.2.1.5 - linux 3.4.91

--- sbuild-jolie.sh
+++ sbuild-jolie.sh
@@ -7,7 +7,7 @@ if ! python --version 2>&1 | grep 2.7 ; then
 fi
 
 BASE_SEMA_VER="Jolie_Kernel_Mako_"
-VER="2.1.1"
+VER="2.1.5"
 SEMA_VER=$BASE_SEMA_VER$VER
 
 #export KBUILD_BUILD_VERSION="2"
--- sbuild.sh
+++ sbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_SEMA_VER="Semaphore_N4_2.1.1"
+BASE_SEMA_VER="Semaphore_N4_2.1.5"
 VER=""
 SEMA_VER=$BASE_SEMA_VER$VER
 
